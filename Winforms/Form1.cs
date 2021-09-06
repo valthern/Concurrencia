ebc803989f308a9e828fb9428e06333875dd4c80
Omar I. Martínez Gómez <omar_238@hotmail.com>
2021-09-06 00:25:54 -0500

    start

---
+++ Winforms/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnIniciar_Click(object sender, EventArgs e)
+        {
+            loadingGIF.Visible = true;
+            await Esperar();
+            MessageBox.Show("Pasaron los 5 segundos");
+            loadingGIF.Visible = false;
+        }
+
+        private async Task Esperar()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(5));
+        }
+    }
+}
