aa104798cb87a806c8adf07e7f0d1acb86000574
Omar I. Martínez Gómez <omar_238@hotmail.com>
2021-09-26 23:47:45 -0500

    02.19 Empezando creando nuevas tareas con task.run

--- Winforms/Form1.cs
+++ Winforms/Form1.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Net.Http;
@@ -27,22 +29,62 @@ namespace Winforms
         private async void btnIniciar_Click(object sender, EventArgs e)
         {
             loadingGIF.Visible = true;
-            await Esperar();
-            var nombre = txtInput.Text;
-            var saludo = await ObtenerSaludo(nombre);
-            MessageBox.Show(saludo);
+
+            var tarjetas = ObtenerTarjetasDeCredito(1000);
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            try
+            {
+                await ProcesarTarjetas(tarjetas);
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            MessageBox.Show($"Operación finalizada en {stopwatch.ElapsedMilliseconds / 1000.0} segundos");
+
             loadingGIF.Visible = false;
+        }
+
+        private async Task ProcesarTarjetas(List<string> tarjetas)
+        {
+            var tareas = new List<Task>();
+
+            foreach (var tarjeta in tarjetas)
+            {
+                var json = JsonConvert.SerializeObject(tarjeta);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var respuestaTask = httpClient.PostAsync($"{apiURL}/tarjetas", content);
+                tareas.Add(respuestaTask);
+            }
+
+            await Task.WhenAll(tareas);
+        }
+
+        private List<string> ObtenerTarjetasDeCredito(int cantidadDeTarjetas)
+        {
+            var tarjetas = new List<string>();
+
+            for (int i = 0; i < cantidadDeTarjetas; i++)
+            {
+                tarjetas.Add(i.ToString().PadLeft(16, '0'));
+            }
+
+            return tarjetas;
         }
 
         private async Task Esperar()
         {
-            await Task.Delay(TimeSpan.FromSeconds(5));
+            await Task.Delay(TimeSpan.FromSeconds(0));
         }
 
         private async Task<string> ObtenerSaludo(string nombre)
         {
             using (var respuesta = await httpClient.GetAsync($"{apiURL}/saludos/{nombre}"))
             {
+                respuesta.EnsureSuccessStatusCode();
                 var saludo = await respuesta.Content.ReadAsStringAsync();
                 return saludo;
             }
